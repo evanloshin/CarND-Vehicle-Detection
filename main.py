77199be0dad46d220f9f0e8870c4ac63a535b493
Evan Loshin <loshinevan@gmail.com>
2018-08-26 22:14:54 -0700

    Add main.py and functions.py rough draft

---
+++ main.py
@@ -0,0 +1,85 @@
+from functions import *
+import matplotlib.image as mpimg
+import matplotlib.pyplot as plt
+import numpy as np
+import cv2
+import time
+import glob
+from sklearn.utils import shuffle
+from sklearn.svm import LinearSVC
+from sklearn.preprocessing import StandardScaler
+from sklearn.model_selection import train_test_split
+
+################### HYPERPARAMETERS ###################
+color_space = 'YUV' # Can be YUV or YCrCb
+orient = 9  # HOG orientations
+pix_per_cell = 8 # HOG pixels per cell
+cell_per_block = 3 # HOG cells per block
+hog_channel = 'ALL' # Can be 0, 1, 2, or "ALL"
+spatial_size = (16, 16) # Spatial binning dimensions
+hist_bins = 16    # Number of histogram bins
+spatial_feat = True # Spatial features on or off
+hist_feat = True # Histogram features on or off
+hog_feat = True # HOG features on or off
+y_start_stop = [400, None] # Min and max in y to search in slide_window()
+scale_factor = 1.2
+heatmap_threshold = 1
+################# END HYPERPARAMETERS #################
+
+# Read in cars and not cars
+cars = glob.glob('data/vehicles/*/*.png')
+notcars = glob.glob('data/non-vehicles/*/*.png')
+
+# Uncomment to reduce the sample size if desired
+# to speed up pipeline
+# cars = shuffle(cars)
+# notcars = shuffle(notcars)
+# sample_size = 2000
+# cars = cars[0:sample_size]
+# notcars = notcars[0:sample_size]
+
+# extract feature vectors from each category of images
+car_features = extract_features(cars, color_conv=color_space,
+                                spatial_size=spatial_size, hist_bins=hist_bins,
+                                orient=orient, pix_per_cell=pix_per_cell,
+                                cell_per_block=cell_per_block,
+                                hog_channel=hog_channel, spatial_feat=spatial_feat,
+                                hist_feat=hist_feat, hog_feat=hog_feat)
+notcar_features = extract_features(notcars, color_conv=color_space,
+                                   spatial_size=spatial_size, hist_bins=hist_bins,
+                                   orient=orient, pix_per_cell=pix_per_cell,
+                                   cell_per_block=cell_per_block,
+                                   hog_channel=hog_channel, spatial_feat=spatial_feat,
+                                   hist_feat=hist_feat, hog_feat=hog_feat)
+
+# Create an array stack of feature vectors
+X = np.vstack((car_features, notcar_features)).astype(np.float64)
+
+# Define the labels vector
+y = np.hstack((np.ones(len(car_features)), np.zeros(len(notcar_features))))
+
+# Split up data into randomized training and test sets
+rand_state = np.random.randint(0, 100)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=rand_state)
+
+# Fit a per-column scalar
+X_scaler = StandardScaler().fit(X_train)
+# Apply the scalar to X
+X_train = X_scaler.transform(X_train)
+X_test = X_scaler.transform(X_test)
+
+# Use a linear SVC
+svc = LinearSVC()
+# Check the training time for the SVC
+t=time.time()
+svc.fit(X_train, y_train)
+t2 = time.time()
+print(round(t2-t, 2), 'Seconds to train SVC...')
+# Check the score of the SVC
+print('Test Accuracy of SVC = ', round(svc.score(X_test, y_test), 4))
+
+test_img = mpimg.imread('test_images/test1.jpg')
+window_img = find_cars(test_img, color_space, y_start_stop[0], y_start_stop[1], scale_factor,
+                       svc, X_scaler, orient, pix_per_cell, cell_per_block, spatial_size, hist_bins, heatmap_threshold)
+plt.imshow(window_img)
+plt.show()
